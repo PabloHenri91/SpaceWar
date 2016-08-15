7d21a4e3cb2aacc84f5a38b1152d07978cd21b8b
Pablo Henrique Bertaco <pablo_fonseca91@icloud.com>
2016-08-15 16:52:26 -0300

    teste

--- Xcode/Game/Game/CoreData/MemoryCard.swift
+++ Xcode/Game/Game/CoreData/MemoryCard.swift
@@ -162,24 +162,7 @@ class MemoryCard {
         do {
             try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: options)
         } catch {
-            // Report any error we got.
-            var dict = [String: AnyObject]()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason
-            
-            dict[NSUnderlyingErrorKey] = error as! NSError
-            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            // Replace this with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
-            
-            try! ALIterativeMigrator.iterativeMigrateURL(url, ofType: NSSQLiteStoreType, toModel: self.managedObjectModel, orderedModelNames: [ "SpaceWar", "SpaceWar 2", "SpaceWar 3", "SpaceWar 4"])
-            
-            coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-            
-            try! coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: options)
-            
-            return coordinator
+            fatalError()
         }
         
         return coordinator
