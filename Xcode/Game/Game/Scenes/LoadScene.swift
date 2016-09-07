1e8a5bfd386963c0f40cde6780835219c45be22a
Paulo Henrique dos Santos <henrique_2601@hotmail.com>
2016-09-07 13:09:08 -0300

    ajuste de merge

--- Xcode/Game/Game/Scenes/LoadScene.swift
+++ Xcode/Game/Game/Scenes/LoadScene.swift
@@ -30,6 +30,7 @@ class LoadScene: GameScene {
 //            MemoryCard.sharedInstance.playerData.points = 1000000
 //            MemoryCard.sharedInstance.playerData.premiumPoints = 1000000
 //            Research.cheatDuration()
+            MemoryCard.sharedInstance.playerData.needBattleTraining = true
         #endif
     }
     
